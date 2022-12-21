0fe07c037d7bf31cf1ed766c56ff5f8c9b42835e
AntonYaroshevskii <120041153+AntonYaroshevskii@users.noreply.github.com>
2022-12-21 12:04:02 +0300

    HomeWork2

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 1
+/*Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+if ((100 <= num) && (num < 1000)) {
+    Console.WriteLine($"{num = (num / 10) % 10 }");
+}
+else{
+    Console.WriteLine($"Ввели не верное число.");
+}new Random().Next(0,100000);*/
+
+//Задача 2
+/*int num = new Random().Next(0,100000);
+Console.WriteLine(num);
+if (num < 100)
+{
+Console.WriteLine($"Третьей цифры нет.");
+}
+if ( (100 <= num) && (num < 1000)){
+    Console.WriteLine($"Терья цифра:{num = (num / 1) % 10 }");
+    
+}
+if ( (999 < num) && (num < 10000)){
+    Console.WriteLine($"Терья цифра:{num = (num / 10) % 10 }");
+    
+}
+
+if ( (9999 < num) && (num < 100000)){
+    Console.WriteLine($"Терья цифра:{num = (num / 100) % 10 }");
+    
+}*/
+
+//Залача 3
+/*Console.WriteLine("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
+if ((0 < num) && (num <6)) 
+{
+    Console.WriteLine($"нет.");
+}
+else if ((5 < num) && (num < 8)) 
+{
+    Console.WriteLine($"да.");
+}
+else 
+{
+    Console.WriteLine($"Такого дня недели нет.");
+}*/
